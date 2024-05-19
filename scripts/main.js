d0952d628693d3326b8c36ba2c16a720dcc70fe9
MiniMapii <anderkendall@hotmail.com>
2024-05-19 02:13:01 +0200

    Udpate HTML
    Ty Javi for all the help

--- scripts/main.js
+++ scripts/main.js
@@ -1,19 +1,21 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const sectionTitles = document.querySelectorAll(".section-title");
-  
-    sectionTitles.forEach(title => {
-      title.addEventListener("click", function() {
-        const sectionContent = this.nextElementSibling;
-  
-        if (sectionContent.classList.contains("active")) {
-          sectionContent.classList.remove("active");
-        } else {
-          document.querySelectorAll(".section-content").forEach(content => {
-            content.classList.remove("active");
-          });
-          sectionContent.classList.add("active");
-        }
-      });
+  const sectionTitles = document.querySelectorAll(".section-title");
+
+  sectionTitles.forEach(title => {
+    title.addEventListener("click", function() {
+      const sectionContent = this.nextElementSibling;
+
+      if (sectionContent.classList.contains("active")) {
+        sectionContent.style.maxHeight = "0"; // Cerramos la sección gradualmente
+        sectionContent.classList.remove("active");
+      } else {
+        document.querySelectorAll(".section-content").forEach(content => {
+          content.style.maxHeight = "0"; // Cerramos todas las demás secciones
+          content.classList.remove("active");
+        });
+        sectionContent.style.maxHeight = sectionContent.scrollHeight + "px"; // Abrimos la sección gradualmente
+        sectionContent.classList.add("active");
+      }
     });
   });
-  
+});
